a8e94033ffb08e1c5839a88af56a972c72aecd76
obetdelacruz <ldelacruz@gbox.adnu.edu.ph>
2023-07-01 17:10:23 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,36 @@
+const pet = "dog";
+if (pet === "lizard") {
+  console.log("I own a lizard");
+} else if (pet === "dog") {
+  console.log("I own a dog");
+} else if (pet === "cat") {
+  console.log("I own a cat");
+} else if (pet === "snake") {
+  console.log("I own a snake");
+} else if (pet === "parrot") {
+  console.log("I own a parrot");
+} else {
+  console.log("I don't own a pet");
+}
+
+const animal = "dog";
+
+switch (pet) {
+  case "lizard":
+    console.log("I own a lizard");
+    break;
+  case "dog":
+    console.log("I own a dog");
+    break;
+  case "cat":
+    console.log("I own a cat");
+    break;
+  case "snake":
+    console.log("I own a snake");
+    break;
+  case "parrot":
+    console.log("I own a parrot");
+    break;
+  default:
+    "i don't own a pet ";
+}
